a06f19f7f6a124e32b3a5d0c2a738749392221ae
angelozangari <angelo.zangari10@gmail.com>
2025-06-10 15:02:22 -0500

    [env] bug fix: correctly filter out already read keys

--- load_key.fish
+++ load_key.fish
@@ -1,41 +1,58 @@
+#!/opt/homebrew/bin/fish
+
 # by default list all the api keys, the installed and the not installed. the not installed have are ordered alphabetically and have a number to their left. if user presses a number that key is added to the .env and the list is updated and shown again. (additionally user can specify a range, specific with comma or * for all.
 
-set -l API_DIR ~/.config/api
+while true
+	clear
+	set -l API_DIR ~/.config/api
 
-## read set 1 of all available keys from ~/.config/api
-for f in $API_DIR/*.env
-	echo (basename $f .env)
-end
-
-## read set 2 of all keys currently present in .env (if any)
-set -l CURRENT_KEYS (grep -o '^[^=]*' .env 2>/dev/null)
-for i in (seq (count $CURRENT_KEYS))
-	set CURRENT_KEYS[$i] (string lower $CURRENT_KEYS[$i] | string replace '_key' '')
-end
-set CURRENT_KEYS (printf "%s\n" $CURRENT_KEYS | sort)
-
-## create set 3 = set 1 - set 2 of available but not yet added
-for k in $AVAILABLE_KEYS
-	if not contains $k $CURRENT_KEYS
-		set REMAINING_KEYS $REMAINING_KEYS $k
+	## read set 1 of all available keys from ~/.config/api
+	set -l AVAILABLE_KEYS
+	for f in $API_DIR/*.env
+		set key (basename $f .env)
+		set AVAILABLE_KEYS $AVAILABLE_KEYS $key
 	end
-end
-set -l REMAINING_KEYS (printf "%s\n" $REMAINING_KEYS | sort)
 
-## print to stdout
-### num. set 3
-for i in (seq (count $REMAINING_KEYS))
-	echo "$i. $REMAINING_KEYS[$i]"
-end
-### set 2
-printf "\nAlready in .env:\n"
-for k in $CURRENT_KEYS
-	echo $k
-end
+	## read set 2 of all keys currently present in .env (if any)
+	set -l RAW_KEYS (grep -o '^[^=]*' .env 2>/dev/null)
 
-## read number (later range, list and *)
-read -p "Enter key number to add: " choice
+	set -l CURRENT_KEYS
+	if test (count $RAW_KEYS) -gt 0
+		for k in $RAW_KEYS
+			set norm (string lower (string trim $k) | string replace '_key' '')
+			set CURRENT_KEYS $CURRENT_KEYS $norm
+		end
+	end
+	set CURRENT_KEYS (printf "%s\n" $CURRENT_KEYS | sort)
 
-## add specified keys to env
-set key $REMAINING_KEYS[$choice]
-cat "$API_DIR/$key.env" >> .env
+	## create set 3 = set 1 - set 2 of available but not yet added
+	set -l REMAINING_KEYS
+	for k in $AVAILABLE_KEYS
+		if not contains $k $CURRENT_KEYS
+			set REMAINING_KEYS $REMAINING_KEYS $k
+		end
+	end
+	set -l REMAINING_KEYS (printf "%s\n" $REMAINING_KEYS | sort)
+
+	## print to stdout
+	### num. set 3
+	for i in (seq (count $REMAINING_KEYS))
+		echo "$i. $REMAINING_KEYS[$i]"
+	end
+	### set 2
+	printf "\nAlready in .env:\n"
+	for k in $RAW_KEYS
+		echo $k
+	end
+
+	## read number (later range, list and *)
+	read -P "Enter key number to add (or q to quit):" choice
+
+	if test "$choice" = "q"
+		break
+	end
+
+	## add specified keys to env
+	set key $REMAINING_KEYS[$choice]
+	cat "$API_DIR/$key.env" >> .env
+end
